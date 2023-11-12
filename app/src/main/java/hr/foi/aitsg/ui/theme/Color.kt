ee1cad12d646cebe40c2b01b723c6fddd451eb51
kglazer4 <92980394+kglazer4@users.noreply.github.com>
2023-11-13 00:42:23 +0100

    Revert "Signal 38 prijava korisnika"

--- app/src/main/java/hr/foi/aitsg/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package hr.foi.aitsg.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Black = Color(0xFF222831)
-val Grey = Color(0xFF393E46)
-val Cyan = Color(0xFF00ADB5)
-val White = Color(0xffEEEEEE)
-val LightGrey = Color(0xFFD4D4D4)
--- core/repository/src/main/java/hr/foi/repository/Repository.kt
+++
@@ -1,5 +0,0 @@
-package hr.foi.repository
-
-interface Repository {
-
-}
