9ff17764112f089d8f12c76d269ef6572a13ad01
Petar Martinović <martinovicp11@gmail.com>
2023-11-28 21:42:02 +0100

    Dozvole

--- app/src/main/java/hr/foi/aitsg/MainActivity.kt
+++ app/src/main/java/hr/foi/aitsg/MainActivity.kt
@@ -195,7 +195,7 @@ class MainActivity : ComponentActivity() {
                         composable("FileScanner"){
                             multiplePermissionResultLauncher.launch(
                                 arrayOf(
-                                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    android.Manifest.permission.READ_EXTERNAL_STORAGE
                                 )
                             )
                             val context = LocalContext.current
