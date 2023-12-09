4546362f20e089acce97c5e48ee14a541689ee89
Viktor-Alojzije Ćorić <vcoric@student.foi.hr>
2023-12-09 12:47:08 +0100

    Modificirani prompt i objekt za spremanje

--- app/src/main/java/hr/foi/aitsg/OpenAIHandler.kt
+++ app/src/main/java/hr/foi/aitsg/OpenAIHandler.kt
@@ -10,28 +10,27 @@ import com.aallam.openai.client.OpenAI
 import org.json.JSONObject
 
 class OpenAIHandler() {
-    val response = OpenAIResponse()
-    val openaiKey = BuildConfig.OPENAI_KEY
-    val openAI = OpenAI(openaiKey)
+    private val openaiKey = BuildConfig.OPENAI_KEY
+    private val openAI = OpenAI(openaiKey)
     val modelId = ModelId("gpt-3.5-turbo-0613")
     private var query = ""
     suspend fun makeQuery(input: String) {
         if(input.startsWith("#include")) {
             query =
-                "U nastavku ti dajem jedan Google test (C++), ti generiraj ime testa, opis testa i izdvoji testne korake. Objasni što se u testu radi i što se želi postići u implementacijskom kodu. Format neka bude pripremljen za objekt OpenAIResponse zapisan u JSON notaciji koji u sebi sadrži atribute name, decription i testSteps. Potreban je samo JSON, nikakvi dodatni komentari. Ovo je kod:\n\n$input"
-            var response = sendQuery(query)
+                "U nastavku ti dajem jedan Google test (C++), ti generiraj ime testa, opis testa i izdvoji testne korake. Objasni što se u testu radi i što se želi postići u implementacijskom kodu. Format neka bude pripremljen za objekt OpenAIResponse zapisan u JSON notaciji koji u sebi sadrži atribute name, decription i testSteps. testSteps je polje stringova, a svaki string je zapisan u CSV obliku gdje je !!! delimiter, a zapis je oblika step!!!description!!!code!!!expectedResult. Potreban je samo JSON, nikakvi dodatni komentari. Ovo je kod:\n\n$input"
+            val response = sendQuery(query)
             CreateResponseObject(response)
         }
         else if(input.startsWith("***")) {
             query =
-                "U nastavku ti dajem jedan Robot Framework test, ti generiraj ime testa, opis testa i izdvoji testne korake. Objasni što se u testu radi i što se želi postići u implementacijskom kodu. Format neka bude pripremljen za objekt OpenAIResponse zapisan u JSON notaciji koji u sebi sadrži atribute name, decription i testSteps. Potreban je samo JSON, nikakvi dodatni komentari. Ovo je kod:\n\n$input"
-            var response = sendQuery(query)
+                "U nastavku ti dajem jedan Robot Framework test, ti generiraj ime testa, opis testa i izdvoji testne korake. Objasni što se u testu radi i što se želi postići u implementacijskom kodu. Format neka bude pripremljen za objekt OpenAIResponse zapisan u JSON notaciji koji u sebi sadrži atribute name, decription i testSteps. testSteps je polje stringova, a svaki string je zapisan u CSV obliku gdje je !!! delimiter, a zapis je oblika step!!!description!!!code!!!expectedResult. Potreban je samo JSON, nikakvi dodatni komentari. Ovo je kod:\n\n$input"
+            val response = sendQuery(query)
             CreateResponseObject(response)
         }
         else {
             query =
-                "U nastavku ti dajem jedan test, ti generiraj ime testa, opis testa i izdvoji testne korake. Objasni što se u testu radi i što se želi postići u implementacijskom kodu. Format neka bude pripremljen za objekt OpenAIResponse zapisan u JSON notaciji koji u sebi sadrži atribute name, decription i testSteps. Potreban je samo JSON, nikakvi dodatni komentari. Ovo je kod:\n\n$input"
-            var response = sendQuery(query)
+                "U nastavku ti dajem jedan test, ti generiraj ime testa, opis testa i izdvoji testne korake. Objasni što se u testu radi i što se želi postići u implementacijskom kodu. Format neka bude pripremljen za objekt OpenAIResponse zapisan u JSON notaciji koji u sebi sadrži atribute name, decription i testSteps. testSteps je polje stringova, a svaki string je zapisan u CSV obliku gdje je !!! delimiter, a zapis je oblika step!!!description!!!code!!!expectedResult. Potreban je samo JSON, nikakvi dodatni komentari. Ovo je kod:\n\n$input"
+            val response = sendQuery(query)
             CreateResponseObject(response)
         }
     }
@@ -51,18 +50,20 @@ class OpenAIHandler() {
     }
 
     private fun CreateResponseObject(response: String) {
+        val responseObject = OpenAIResponse()
         val jsonObject = JSONObject(response)
-        OpenAIResponse().name = jsonObject.getString("name")
-        OpenAIResponse().description = jsonObject.getString("description")
+        responseObject.name = jsonObject.getString("name")
+        responseObject.description = jsonObject.getString("description")
         val testStepsArray = jsonObject.getJSONArray("testSteps")
         val testSteps = mutableListOf<String>()
         for (i in 0 until testStepsArray.length()) {
-            val stepObject = testStepsArray.getJSONObject(i)
-            val step = stepObject.getInt("step")
-            val stepDescription = stepObject.getString("description")
-            val code = stepObject.getString("code")
-            testSteps.add("$step!!!$stepDescription!!!$code")
+            testSteps.add(testStepsArray.getString(i))
         }
-        OpenAIResponse().testSteps = testSteps
+        responseObject.testSteps = testSteps
+        writeInDatabase(responseObject)
+    }
+
+    private fun writeInDatabase(responseObject: OpenAIResponse) {
+
     }
 }
