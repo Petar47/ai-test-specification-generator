63511c7fb264a7b2e269091d2b2fe29cbcc5997d
Iva Kustura <ikustura@foi.hr>
2024-01-08 23:27:38 +0100

    - dodana linija : @SuppressLint("SuspiciousIndentation")

--- app/src/main/java/hr/foi/aitsg/ReportsCounter.kt
+++ app/src/main/java/hr/foi/aitsg/ReportsCounter.kt
@@ -1,9 +1,11 @@
 package hr.foi.aitsg
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.runtime.Composable
 import hr.foi.database.DataViewModel
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 fun CountReportsOfProject(dataViewModel : DataViewModel, id_project: Int) : Int{
   var numberOfProjectReports = getNumberOfProjectReports(dataViewModel, id_project)
